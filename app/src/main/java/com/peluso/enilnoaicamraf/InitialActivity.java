4740e265f7e35718fa6911d42dddf0735f5bae0d
ProjetoFamaciaOnline <projetofarmaciaonline@gmail.com>
2018-09-20 21:57:02 -0300

    Primeiro commit

---
+++ app/src/main/java/com/peluso/enilnoaicamraf/InitialActivity.java
@@ -0,0 +1,14 @@
+package com.peluso.enilnoaicamraf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InitialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
